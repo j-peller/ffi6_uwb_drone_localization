3015ad346614ff037984aaab56fab9879f862b6b
Joshua Peller <Joshua.Peller@hotmail.de>
2025-04-30 17:31:23 +0200

    Predefined DWM Modes

---
+++ Drone/dwm1000_control/inc/dw1000_modes.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef DW1000_MODES_H
+#define DW1000_MODES_H
+
+#include "dwm1000_ctrl.hpp"
+
+// Deklaration der konstanten Konfiguration
+extern const dw1000_mode_t THOTRO110;
+
+
+#endif
--- Drone/dwm1000_control/inc/dwm1000_ctrl.hpp
+++ Drone/dwm1000_control/inc/dwm1000_ctrl.hpp
@@ -51,46 +51,50 @@ typedef struct {
 /**
  * 
  */
-struct Channel {
-    uint8_t num;
+typedef struct {
     uint8_t rf_rxctrlh; /* see Table 37 for 0x28:0B– RF_RXCTRLH*/
-    uint32_t rf_txctrl; //TODO Len is 3 in manual?
+    uint32_t rf_txctrl; 
     uint8_t tc_pgdelay;
-    uint32_t fs_pllcfg; //TODO Len is 5 in FS_PLLCFG_LEN?
-};
-
+    uint32_t fs_pllcfg;
+} dw1000_channel_t;
 
 /**
  * 
  */
-struct PRF {
+typedef struct {
     uint32_t txprf;
     uint32_t rxfprf;
-    uint16_t drx_tune1a;
-};
+} dw1000_prf_t;
 
 
 /**
  * 
  */
-struct Tuning {
+typedef struct {
     uint16_t drx_tune0b;
     uint16_t drx_tune1a;
     uint16_t drx_tune1b;
     uint32_t drx_tune2;
-    uint16_t agc_tune1;
-    uint32_t agc_tune2;
     uint8_t fs_plltune;
-};
+} dw1000_tuning_t;
 
 
 /**
  * 
  */
-struct Bitrate {
-    uint32_t txbr;
-    uint32_t rxm110k; //RXM110K
-};
+typedef struct {
+    uint16_t agc_tune1;
+    uint32_t agc_tune2;
+} dw1000_fixed_config_t;
+
+
+/**
+ * 
+ */
+typedef struct {
+    uint32_t txbr;      //< tx_fctrl register
+    uint32_t rxm110k;   //< sys_cfg register
+} dw1000_bitrate_t;
 
 
 /**
@@ -101,20 +105,17 @@ enum SFD {
     DecaWave,
 };
 
-
-/**
- * 
- */
-struct Mode {
-    Channel channel;
-    PRF prf;
-    Bitrate bitrate;
-    uint32_t preamble_code;
-    uint32_t preamble_length;
-    SFD sfd;
-    Tuning tune;
-};
-
+typedef struct {
+    uint8_t channel_num;
+    dw1000_channel_t        channel_config;
+    dw1000_tuning_t         tune_config;
+    dw1000_fixed_config_t   fixed_config;
+    dw1000_prf_t            prf_config;
+    dw1000_bitrate_t        bitrate_config;
+    uint32_t                preamble_code;
+    uint32_t                preamble_length;
+    SFD                     sfd;
+} dw1000_mode_t;
 
 
 /**
@@ -128,7 +129,7 @@ public:
 
     /* DW1000 Configuration */
     dwm_com_error_t do_init_config();
-    dwm_com_error_t set_mode(Mode mode);
+    dwm_com_error_t set_mode(dw1000_mode_t mode);
 
     /* Transmission */
     dwm_com_error_t write_transmission_data(uint8_t* data, uint8_t len);

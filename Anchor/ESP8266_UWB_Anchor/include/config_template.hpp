796c8a3704769eb864399c94ddc6ecc3cf69caf5
Joshua Peller <Joshua.Peller@hotmail.de>
2025-05-22 12:56:15 +0200

    ESP not needed anymore

--- Anchor/ESP8266_UWB_Anchor/include/config_template.hpp
+++
@@ -1,9 +0,0 @@
-/* Rename this file to config.hpp */
-#define SSID	"your_ssid"
-#define PWD		"your__wifi_password"
-
-#define SERVER_IP "XXX.XXX.XXX.XXX" // your webserver ip address
-#define SERVER_PORT 5000            // your webserver port address
-
-#define TIMEOUT 10000 //in ms
-#define AUTOMATIC_RECONNECT true
